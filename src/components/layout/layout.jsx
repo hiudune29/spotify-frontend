6674b0ad0be8d704130b245f1d8814887c366a1a
triet3 <trietpt09@gmail.com>
2025-03-31 22:44:57 +0700

    minhtriet update contentplaylist

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -3,15 +3,16 @@ import { Outlet } from "react-router-dom";
 import PlaylistContent from "../playlist/playlistcontent";
 import Rightbar from "../sidebar/rightbar/rightbar";
 import Sidebar from "../sidebar/leftbar/sidebar";
-
+import ContentPlaylist from "../listContent/contentPlaylist";
 const Layout = () => {
   return (
     <div className="flex h-screen bg-black text-white">
       <Sidebar />
-      <div className="flex-1">
-        <PlaylistContent />
+      <div className="flex-1 overflow-auto">
+        {/* <PlaylistContent /> */}
+        <ContentPlaylist />
       </div>
-      {/* <Rightbar /> */}
+      <Rightbar />
     </div>
   );
 };
